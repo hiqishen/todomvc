2aebf06c435f0697400d8650c0ce0aaaf15df779
zyz <zyzhou17@163.com>
2020-07-26 17:54:42 +0800

    todolist

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
-(function (window) {
-	'use strict';
-
+;(function () {
+	const todos=[
+		{
+			id:"1",
+			title:"吃饭",
+			completed:true
+		},
+		{
+			id:"2",
+			title:"睡觉",
+			completed:false
+		},
+		{
+			id:"3",
+			title:"打豆豆",
+			completed:true
+		},
+	]
 	// Your starting point. Enjoy the ride!
+	//todos es6语法key value一样可以简写
+	const app=new Vue({
+		data:{
+			todos
+		}
+	}).$mount("#app")
 
-})(window);
+})();
