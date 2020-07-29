edf1a78b9a2a2e7b3ccd19b18709dec2ef9e111c
zyz <zyzhou17@163.com>
2020-07-29 23:03:07 +0800

    add focus directive

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 ;(function () {
-	const todos=[
-		{
-			id:1,
-			title:"吃饭",
-			completed:true
-		},
-		{
-			id:2,
-			title:"睡觉",
-			completed:false
-		},
-		{
-			id:3,
-			title:"打豆豆",
-			completed:true
-		},
-	]
+
 	// Your starting point. Enjoy the ride!
 	//todos es6语法key value一样可以简写
+
+
+	Vue.directive('first-focus',{
+		inserted(el,binding){
+			// console.log("qwe");
+			el.focus()
+		}
+	})
+	Vue.directive("edit-focus",{
+		update(el,binding) {
+			// 多个dom同时focus只有第一个有效
+			// el.focus()
+			// console.log(binding.value);
+			if(binding.value){
+				el.focus()
+			}
+		},
+	})
+
+
 	const app=new Vue({
 		data:{
-			todos,
-			currentEditing:null
+			todos:JSON.parse(window.localStorage.getItem("todos")||'[]'),
+			currentEditing:null,
+			currentHash:""
 		},
 		methods:{
 
@@ -44,11 +49,11 @@
 				e.target.value=""
 			},
 
-			handleToggleAll(e){
-				this.todos.forEach(value=>{
-					value.completed=e.target.checked
-				})
-			},
+			// handleToggleAll(e){
+			// 	this.todos.forEach(value=>{
+			// 		value.completed=e.target.checked
+			// 	})
+			// },
 
 			handleRemoveTodo(index){
 				this.todos.splice(index,1)
@@ -69,7 +74,59 @@
 				this.currentEditing=null
 			}
 
+		},
+		computed:{
+			remainingCount(){
+				return this.todos.filter(item=>!item.completed).length
+			},
+			toggleAllStat:{
+				get(){
+					return this.todos.every(i=>i.completed)
+				},
+				set(value){
+					console.log("value",value);
+					// 都可以
+					// const stat=!this.toggleAllStat
+					const stat=value
+					this.todos.forEach(i=>i.completed=stat)
+				}
+			},
+			hashTodos(){
+				switch (this.currentHash) {
+					case "active":
+						return this.todos.filter(i=>!i.completed)
+						break;
+					case "completed":
+						return this.todos.filter(i=>i.completed)
+						break;
+					default:
+						return this.todos
+						break;
+				}
+			}
+		},
+
+		watch:{
+			todos:{
+				handler(val,oldval){
+					// console.log(val[0].completed,oldval[0].completed);
+				  // console.log('222');
+					window.localStorage.setItem("todos",JSON.stringify(this.todos))
+					// console.log(	window.localStorage.getItem("todos"));
+				},
+				deep:true,
+				immediate:true
+			}
 		}
 	}).$mount("#app")
 
+	
+
+
+	window.onhashchange=handleHashChange
+
+	handleHashChange()
+	function handleHashChange(){
+		app.currentHash=window.location.hash.substr(2)
+	}
 })();
